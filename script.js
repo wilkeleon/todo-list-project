96406da27785f1af0886391fb5411f062f6a9107
Wilke Leon <wleonwilke@gmail.com>
2024-07-07 21:38:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let items = [];
+
+const itemsDiv = document.getElementById("items");
+
+function renderItems() {
+  itemsDiv.innerHTML = null;
+
+  for (const [idx, item] of Object.entries(items)) {
+    const container = document.createElement("div");
+    container.style.marginBottom = "10px";
+
+    const text = document.createElement("p");
+    text.style.display = "inline";
+    text.style.marginRight = "10px";
+    text.textContent = item;
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.onclick = () => removeItem(idx);
+
+    container.appendChild(text);
+    container, appendChild(button);
+
+    itemsDiv.appendChild(container);
+  }
+}
+
+function loadItems() {}
+
+function saveItems() {}
+
+function addItem() {}
+
+function removeItem(idx) {
+  items.splice(idx, 1);
+  renderItems();
+}
